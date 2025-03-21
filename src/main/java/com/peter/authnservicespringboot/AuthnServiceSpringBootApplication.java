c5f084131eb23af762bd5a2478f4efb1c6787cee
PeterChen <sanpeter992000@gmail.com>
2025-03-21 19:52:42 +0800

    Initialize Spring Boot project with Maven setup and basic configuration.

---
+++ src/main/java/com/peter/authnservicespringboot/AuthnServiceSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.peter.authnservicespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthnServiceSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthnServiceSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/peter/authnservicespringboot/AuthnServiceSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peter.authnservicespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthnServiceSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
